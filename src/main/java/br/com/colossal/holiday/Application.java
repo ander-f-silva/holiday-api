e6ce9c1e80d25ea3bab6755ea07e00218d42612d
Anderson Silva <infoander@gmail.com>
2025-03-05 23:49:50 -0300

    fix: :bug: Adjusted the config for path redoc

--- src/main/java/br/com/colossal/holiday/Application.java
+++ src/main/java/br/com/colossal/holiday/Application.java
@@ -1,14 +1,14 @@
 package br.com.colossal.holiday;
 
 import io.micronaut.runtime.Micronaut;
-import io.swagger.v3.oas.annotations.*;
-import io.swagger.v3.oas.annotations.info.*;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @OpenAPIDefinition(
-    info = @Info(
-            title = "holiday-api",
-            version = "0.0"
-    )
+        info = @Info(
+                title = "holiday-api",
+                version = "1.0.0"
+        )
 )
 public class Application {
 
